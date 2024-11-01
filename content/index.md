9cdf90fc82995b95b044633af57e0c9566892761
Zain Sikand <121804603+Zainothy@users.noreply.github.com>
2024-11-01 17:42:46 +0000

    Fix

--- content/index.md
+++ content/index.md
@@ -1,5 +1,4 @@
 ---
-title: 
 share: "true"
 ---
 
--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://zainothy.github.io/HSRQuartz/",
+    baseUrl: "https://zainothy.github.io/HSRQuartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
