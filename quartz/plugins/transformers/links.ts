6a8d0850ce34b32e1c8f61441ec077386f2c56f8
Zain <121804603+Zainothy@users.noreply.github.com>
2024-11-03 10:37:17 +0000

    Update links.ts

--- quartz/plugins/transformers/links.ts
+++ quartz/plugins/transformers/links.ts
@@ -25,7 +25,7 @@ interface Options {
 }
 
 const defaultOptions: Options = {
-  markdownLinkResolution: "shortest",
+  markdownLinkResolution: "relative",
   prettyLinks: false,
   openLinksInNewTab: false,
   lazyLoad: false,
