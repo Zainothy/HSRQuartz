dc34e02f7eb949c6b190467088a477dc01957108
Zain <121804603+Zainothy@users.noreply.github.com>
2024-11-03 11:11:42 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Villagers' HSR Guides",
+    pageTitle: "Villager's HSR Guides",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
