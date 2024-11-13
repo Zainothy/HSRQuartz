e3e6cfa7fe2049e96cb1e2154dd7fe8326360b77
Zainothy <zain@sikand.org>
2024-11-13 20:54:47 +0000

    update quartz.layout.ts: accordance with component.mobileonly.TOC
    Allow table of contents display before header on Mobile devices below that of tablet sizing

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,6 +16,7 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.MobileOnly(Component.TableOfContents()),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
